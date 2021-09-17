7a9c729a01bfa48d2b03621b4599dea2c5440176
sairiGitHub <safiri@163.com>
2021-09-17 17:38:47 +0800

    commit test12

--- 第11章  字符串和字符串函数/test/test10.c
+++ 第11章  字符串和字符串函数/test/test10.c
@@ -4,16 +4,85 @@
 该程序应该应用该函数只每个输入的字符串，并显示处理后的字符串。
  */
 #include <stdio.h>
+#define SIZE 30
 
 char * trimSpace(char *string);
+char *s_gets(char *st, int n);
 
 int main(void)
 {   
-
+    char str[SIZE]; 
+    puts("输入个字符串去除里面的空格:");
+    s_gets(str, SIZE);
+    while (*str != 'q')
+    {
+        trimSpace(str);
+        printf("去除空格后字符串为：%s\n", str);
+        puts("输入个字符串去除里面的空格:");
+        s_gets(str, SIZE);
+    }
+    
     return 0;
 }
 
-char * trimSpace(char *string)
+char * trimSpace(char *str)
 {
-    
+    int count = 0;
+    while (*(str+count) != '\0')
+    {
+        if (*(str+count) == ' ' || *(str+count) == '\t')
+        {
+            int move = count;
+            do
+            { /* 移动了字符串可能造成时间浪费 */  
+                *(str+move) = *(str+move+1);
+                move++;
+            } while (*(str+move) != '\0');
+        }
+        else
+        {
+            count++;
+        }
+    }
+    return str;
+}
+
+/* 效率比第一种高 */
+char * trimSpace2(char *str)
+{
+    char *head = str;
+    int count = 0;
+    while (*str != '\0')
+    {
+        if (*str != ' ')
+            *head++ = *str++;
+        else
+        {
+            str++;
+            count++;
+        }
+    }
+    while (count--)
+    {
+        *head++ = '\0';
+    }
+    return str;
+}
+
+char *s_gets(char *st, int n)
+{
+    char *ret_val;
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        int i = 0;
+        while (st[i] != '\n' && st[i] != '\0')
+            i++;
+        if (st[i] == '\n')
+            st[i] = '\0';
+        else
+            while (getchar() != '\n')
+                continue;            
+    }
+    return ret_val;
 }
--- 第11章  字符串和字符串函数/test/test12.c
+++ 第11章  字符串和字符串函数/test/test12.c
@@ -2,3 +2,104 @@
 12.编写一个程序，读取输入，直至读到 EOF，报告读入的单词数、大 
 写字母数、小写字母数、标点符号数和数字字符数。使用ctype.h头文件中的 函数。
  */
+#include <stdio.h>
+#include <ctype.h>
+
+#define SIZE 80
+int check_words(char *string);
+int check_upper(char *string);
+int check_lower(char *string);
+int check_punct(char *string);
+int check_digit(char *string);
+
+int main(void)
+{
+    char in[SIZE];
+    int i = 0;
+    while ((in[i] = getchar()) != EOF)
+    {
+        i++;
+        if (i >= SIZE)
+        {
+            puts("Over flowed.");
+            break;
+        }
+    }
+
+    puts("You input complete.\n Now let's counting.");
+    printf("Input words %d.\n", check_words(in));
+	printf("Input upper char %d.\n", check_upper(in));
+	printf("Input lower char %d.\n", check_lower(in));
+	printf("Input punct char %d.\n", check_punct(in));
+	printf("Input digital %d.\n", check_digit(in));
+
+    return 0;
+}
+
+int check_words(char *string)
+{
+    int count = 0;
+    int start = 0;
+    while (*string != EOF)
+    {
+        if (isalpha(*string) == 0 && start == 0)
+        {
+            string++;
+        }
+        else if (isalpha(*string) == 0 && start == 1)
+        {
+            string++;
+            count++;
+            start = 0;
+        }
+        else if (isalpha(*string) != 0)
+        {
+            string++;
+            start = 1;
+        }
+    }
+    if (start == 1)
+        count++;
+    return count;        
+}
+
+int check_upper(char *string)
+{
+    int count = 0;
+    while (*string != EOF)
+    {
+        if (isupper(*string++) != 0)
+            count++;        
+    }
+    return count;
+}
+int check_lower(char *string)
+{
+    int count = 0;
+    while (*string != EOF)
+    {
+        if (islower(*string++) != 0)
+            count++;        
+    }
+    return count;
+}
+int check_punct(char *string)
+{
+    int count = 0;
+    while (*string != EOF)
+    {
+        if (ispunct(*string++) != 0)
+            count++;        
+    }
+    return count;
+}
+int check_digit(char *string)
+{
+    int count = 0;
+    while (*string != EOF)
+    {
+        if (isdigit(*string++) != 0)
+            count++;        
+    }
+    return count;
+}
